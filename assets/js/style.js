2dfe53d53b7b4446c579daadbfdc25d4bf7512e4
BagjaAb21 <bagja.basith0612@students.amikom.ac.id>
2023-12-11 23:25:22 +0700

    third

---
+++ assets/js/style.js
@@ -0,0 +1,15 @@
+function showFAQ(element) {
+    const content = element.querySelector(".faq-content");
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  }  
+  
+  document.addEventListener("click", function(event) {
+    if (event.target.classList.contains("faq-item")) {
+      showFAQ(event.target);
+    }
+  });
+  
